75a45e4d7bbdc51a3042f4ea7437a588e3a03b0c
sachinopboy2 <sachin25u7@gmail.com>
2025-07-30 15:06:34 +0530

    Update Youtube.py

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -15,9 +15,9 @@ from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
-from AnonMusic import LOGGER
-from AnonMusic.utils.database import is_on_off
-from AnonMusic.utils.formatters import time_to_seconds
+from BrandrdXMusic import LOGGER
+from BrandrdXMusic.utils.database import is_on_off
+from BrandrdXMusic.utils.formatters import time_to_seconds
 from config import BASE_API_KEY,  BASE_API_URL
 
 logger = LOGGER(__name__)
