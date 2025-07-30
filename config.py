c3fcc3a5dbbc73182f16d4cebd2bc585e6b36bb7
sachinopboy2 <sachin25u7@gmail.com>
2025-07-30 14:55:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/naynamusicbot/smartshizuka",
+    "https://github.com/sachinopboy2/Nayana",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
